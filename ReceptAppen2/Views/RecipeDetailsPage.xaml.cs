ec5400f636f7ded6d809b76ff57da966f466fa08
fruniia <annanilssonniia@gmail.com>
2023-03-09 10:04:02 +0100

    Added Button
    Added a Backbutton and changed some GUI

--- ReceptAppen2/Views/RecipeDetailsPage.xaml.cs
+++ ReceptAppen2/Views/RecipeDetailsPage.xaml.cs
@@ -8,4 +8,8 @@ public partial class RecipeDetailsPage : ContentPage
         BindingContext = new DetailsRecipeViewModel(recipe);
 	}
 
+    private async void OnClickedGoBack(object sender, EventArgs e)
+    {
+        await Navigation.PopAsync();
+    }
 }
