fc6919e16f1e5f7e4831fdc01df89e34406dc262
Gr2dd3 <gradde23@gmail.com>
2023-03-15 16:04:39 +0100

    cleaned in RecipeSearchService. Left earlier code in comments. Remove? It seems to work

--- ReceptAppen2/Services/RecipeSearchService.cs
+++ ReceptAppen2/Services/RecipeSearchService.cs
@@ -6,14 +6,16 @@
         public static async Task<RecipeSearch> GetRecipeSearchAsync(string searchPhrase)
         {
 
-            // Request
-            var client = new HttpClient();
-            var request = new HttpRequestMessage(HttpMethod.Get, $"https://handla.api.ica.se/api/recipes/searchwithfilters?phrase={searchPhrase}&recordsPerPage=20&pageNumber=1&sorting=0");
-            request.Headers.Add("Authorization", "Basic " + SessionsData.AuthorizationKey);
-            request.Headers.Add("AuthenticationTicket", SessionsData.AuthenticationTicket);
+            //// Request
+            //var client = new HttpClient();
+            //var request = new HttpRequestMessage(HttpMethod.Get, $"https://handla.api.ica.se/api/recipes/searchwithfilters?phrase={searchPhrase}&recordsPerPage=20&pageNumber=1&sorting=0");
+            //request.Headers.Add("Authorization", "Basic " + SessionsData.AuthorizationKey);
+            //request.Headers.Add("AuthenticationTicket", SessionsData.AuthenticationTicket);
 
-            // Response
-            var response = await client.SendAsync(request);
+            //// Response
+            //var response = await client.SendAsync(request);
+
+            var response = await GetApiRequest($"/api/recipes/searchwithfilters?phrase={searchPhrase}&recordsPerPage=20&pageNumber=1&sorting=0", true);
             response.EnsureSuccessStatusCode();
             RecipeSearch recipeSearch = new();
             if (response is not null)
@@ -26,12 +28,14 @@
 
         public static async Task<RecipeSearch> GetRandomRecipesAsync()
         {
-            // Request
-            var client = new HttpClient();
-            var request = new HttpRequestMessage(HttpMethod.Get, "https://handla.api.ica.se/api/recipes/random?numberofrecipes=6");
-            
-            // Response
-            var response = await client.SendAsync(request);
+            //// Request
+            //var client = new HttpClient();
+            //var request = new HttpRequestMessage(HttpMethod.Get, "https://handla.api.ica.se/api/recipes/random?numberofrecipes=6");
+
+            //// Response
+            //var response = await client.SendAsync(request);
+
+            var response = await GetApiRequest("/api/recipes/random?numberofrecipes=6", false);
             response.EnsureSuccessStatusCode();
             RecipeSearch recipeSearch = new();
             if (response is not null)
@@ -45,13 +49,17 @@
 
         public static async Task<Recipe> GetOneRecipeAsync(int recipeId)
         {
-            // Request
-            var client = new HttpClient();
-            var request = new HttpRequestMessage(HttpMethod.Get, "https://handla.api.ica.se/api/recipes/recipe/" + recipeId);
+            //// Request
+            //var client = new HttpClient();
+            //var request = new HttpRequestMessage(HttpMethod.Get, "https://handla.api.ica.se/api/recipes/recipe/" + recipeId);
 
-            // Response
-            var response = await client.SendAsync(request);
+            //// Response
+            //var response = await client.SendAsync(request);
+
+
+            var response = await GetApiRequest("/api/recipes/recipe/" + recipeId, false);
             response.EnsureSuccessStatusCode();
+
             Recipe recipe = new();
             if (response is not null)
             {
@@ -59,6 +67,21 @@
                 recipe = JsonSerializer.Deserialize<Recipe>(responseString);
             }
             return recipe;
+        }
+
+        private static async Task<HttpResponseMessage> GetApiRequest(string uri, bool HasHeaders)
+        {
+            var client = new HttpClient();
+            var request = new HttpRequestMessage(HttpMethod.Get, "https://handla.api.ica.se" + uri);
+
+            if (HasHeaders)
+            {
+                request.Headers.Add("Authorization", "Basic " + SessionsData.AuthorizationKey);
+                request.Headers.Add("AuthenticationTicket", SessionsData.AuthenticationTicket);
+            }
+            var response = await client.SendAsync(request);
+
+            return response;
         }
     }
 }
